3cb6c0597e1cf3b2f8c8555d667daaa73cf1551f
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-03-02 16:46:19 -0500

    commit tree e06cadd07ff094d9fa930e09bfe493bdedfa8303

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Toggle() {
-  return <button>OFF</button>;
+  
+  const [isOn, setIsOn] = useState(false); 
+  //call useState and provide an initial value = false bc btn should be off when component first renders
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn)
+  }
+  //call the setter function setIsOn to update our state var
+
+  const color = isOn ? 'salmon' : 'white';
+
+  
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? 'ON' : 'OFF'}
+    </button> 
+  )
+  //conditional rendering to dynamically determine btn's text based on state variable 
+
 }
 
 export default Toggle;
